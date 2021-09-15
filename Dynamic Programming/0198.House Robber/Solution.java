772fe8c8bc0ef44d674d054a743e135e20c52323
hongyiheng <hongyihengg@gmail.com>
2021-09-15 16:48:39 +0800

    feat: No.0198 Solution (DP)

--- Dynamic Programming/0198.House Robber/Solution.java
+++ Dynamic Programming/0198.House Robber/Solution.java
@@ -1,17 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 1) {
+        int len = nums.length;
+        if (len == 1) {
             return nums[0];
         }
-        if (nums.length == 2) {
-            return Math.max(nums[0], nums[1]);
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < len; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
         }
-        int[] res = new int[nums.length];
-        res[0] = nums[0];
-        res[1] = Math.max(nums[0], nums[1]);
-        for (int i = 2; i < nums.length; i++) {
-            res[i] = Math.max(res[i - 2] + nums[i], res[i - 1]);
-        }
-        return res[res.length - 1];
+        return dp[len - 1];
     }
 }
