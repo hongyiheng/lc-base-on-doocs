1770c9f702b4296ec0ca63f0c8661ebe9878af1c
hongyiheng <hongyihengg@gmail.com>
2021-11-20 01:10:53 +0800

    docs: vue press

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    lang: 'zh-CN',
+    title: 'LeetCode Solution Record',
+    description: 'LeetCode Solution Record',
+    head: [['link', { rel: 'icon', href: 'https://vuejs.org/images/logo.png' }]],
+    themeConfig: {
+        logo: 'https://vuejs.org/images/logo.png',
+        repo: 'https://github.com/hongyiheng/lc-base-on-doocs',
+        sidebar: [
+            '/Array/0001.Two%20Sum/README.md',
+        ]
+    },
+}
