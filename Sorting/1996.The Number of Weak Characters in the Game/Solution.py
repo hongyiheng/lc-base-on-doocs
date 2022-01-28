7968db3a2aec4ea20fbd8ec684f57bfddc9043eb
hongyiheng <hongyihengg@gmail.com>
2022-01-28 22:07:36 +0800

    feat: No.1996.The Number of Weak Characters in the Game (Sorting)

--- Sorting/1996.The Number of Weak Characters in the Game/Solution.py
+++ Sorting/1996.The Number of Weak Characters in the Game/Solution.py
@@ -2,9 +2,9 @@ class Solution:
     def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
         properties.sort(key=lambda x: (-x[0], x[1]))
         ans, maxDefense = 0, -1
-        for i in range(len(properties)):
-            if maxDefense > properties[i][1]:
+        for v in properties:
+            if maxDefense > v[1]:
                 ans += 1
             else:
-                maxDefense = properties[i][1]
+                maxDefense = v[1]
         return ans
