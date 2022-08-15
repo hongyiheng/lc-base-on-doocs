b01a78f09da48c5b86fae1f94c5f488d5b0b7080
hongyiheng <hongyihengg@gmail.com>
2022-08-15 22:43:05 +0800

    feat: NO.0025.Reverse Nodes in k-Group (Linked List)

--- Linked List/0025.Reverse Nodes in k-Group/Solution.py
+++ Linked List/0025.Reverse Nodes in k-Group/Solution.py
@@ -5,23 +5,19 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        dummy = ListNode()
-        cur = dummy
-        while head:
-            q = deque()
-            cnt = k
-            while head and cnt:
-                q.append(head)
-                head = head.next
-                cnt -= 1
-            if cnt:
-                while q:
-                    cur.next = q.popleft()
-                    cur = cur.next
-            else:
-                while q:
-                    cur.next = q.pop()
-                    cur = cur.next
-            if not head:
-                cur.next = None
-        return dummy.next
+        if k == 1:
+            return head
+        tail, tmp = head, head
+        vals = []
+        cnt = 0
+        while tail:
+            vals.append(tail.val)
+            cnt += 1
+            if cnt == k:
+                while vals:
+                    tmp.val = vals.pop()
+                    tmp = tmp.next
+                cnt = 0
+                tmp = tail.next
+            tail = tail.next
+        return head
