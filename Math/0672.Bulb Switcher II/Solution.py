9c207a38fbf8226864f63c45fa838b5e0725c0a4
hongyiheng <hongyihengg@gmail.com>
2022-09-15 23:12:57 +0800

    feat: NO.0672.Bulb Switcher II (Math)

---
+++ Math/0672.Bulb Switcher II/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def flipLights(self, n: int, presses: int) -> int:
+        if presses == 0:
+            return 1
+        if n == 1:
+            return 2
+        if n == 2:
+            return 3 if presses == 1 else 4
+        if presses == 1:
+            return 4
+        return 7 if presses == 2 else 8
