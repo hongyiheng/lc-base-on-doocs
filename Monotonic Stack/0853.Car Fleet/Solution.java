03d31685c332688cbf9a908474f32399308007f6
hongyiheng <hongyihengg@gmail.com>
2023-08-03 16:34:24 +0800

    fix: NO.0853.Car Fleet (Monotonic Stack)

--- Monotonic Stack/0853.Car Fleet/Solution.java
+++ Monotonic Stack/0853.Car Fleet/Solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        int ans = 0, n = position.length;
+        int n = position.length;
         int[][] ps = new int[n][2];
         for (int i = 0; i < n; i++) {
             ps[i][0] = position[i];
@@ -10,14 +10,10 @@ class Solution {
         Deque<Double> q = new ArrayDeque<>();
         for (int i = n - 1; i > -1; i--) {
             double t = (target - ps[i][0]) * 1.0 / ps[i][1];
-            while (!q.isEmpty() && q.peekLast() < t) {
-                q.pollLast();
+            if (q.isEmpty() || q.peekLast() < t) {
+                q.addLast(t);
             }
-            if (q.isEmpty()) {
-                ans++;
-            }
-            q.addFirst(t);
         }
-        return ans;
+        return q.size();
     }
 }
--- Monotonic Stack/0853.Car Fleet/Solution.py
+++ Monotonic Stack/0853.Car Fleet/Solution.py
@@ -1,14 +1,10 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        ans, n = 0, len(position)
         ps = [[p, s] for p, s in zip(position, speed)]
         ps.sort()
         q = deque()
         for i in range(len(position) - 1, -1, -1):
             t = (target - ps[i][0]) / ps[i][1]
-            while q and q[-1] < t:
-                q.pop()
-            if not q:
-                ans += 1
-            q.append(t)
-        return ans
+            while not q or q[-1] < t:
+                q.append(t)
+        return len(q)
