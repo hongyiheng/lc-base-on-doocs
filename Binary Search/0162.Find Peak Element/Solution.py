9d7c7a8ae63421f32af1f7918074e65ba854c5d8
hongyiheng <hongyihengg@gmail.com>
2023-12-18 15:32:25 +0800

    fix: NO.0162.Find Peak Element (Binary Search)

--- Binary Search/0162.Find Peak Element/Solution.py
+++ Binary Search/0162.Find Peak Element/Solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        left, right = 0, len(nums) - 1
-        while (left < right):
-            mid = (left + right) >> 1
-            if nums[mid] >= nums[mid + 1]:
-                right = mid
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) >> 1
+            if nums[mid] < nums[mid + 1]:
+                l = mid + 1
             else:
-                left = mid + 1
-        return left
+                r = mid
+        return l
