d37be2d8f432ffd8afed7bff00e2699572e8b14b
hongyiheng <hongyihengg@gmail.com>
2024-09-01 22:32:19 +0800

    feat: NO.1450.Number of Students Doing Homework at a Given Time (Array)

--- Array/1450.Number of Students Doing Homework at a Given Time/Solution.java
+++ Array/1450.Number of Students Doing Homework at a Given Time/Solution.java
@@ -1,19 +1,12 @@
 class Solution {
     public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
-        int[] t = new int[1010];
+        int[] q = new int[1010];
         for (int v : startTime) {
-            t[v]++;
+            q[v]++;
         }
         for (int v : endTime) {
-            t[v + 1]--;
+            q[v + 1]--;
         }
-        int ans = 0;
-        for (int i = 0; i < 1010; i++) {
-            ans += t[i];
-            if (i == queryTime) {
-                break;
-            }
-        }
-        return ans;
+        return Arrays.stream(q).limit(queryTime + 1).sum();
     }
 }
