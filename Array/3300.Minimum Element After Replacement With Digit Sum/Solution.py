e5fd4c519221950cc693f1345ea9d5293355b07a
hongyiheng <hongyihengg@gmail.com>
2024-11-15 23:32:09 +0800

    feat: No.3300.Minimum Element After Replacement With Digit Sum (Array)

---
+++ Array/3300.Minimum Element After Replacement With Digit Sum/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countOfSubstrings(self, word: str, k: int) -> int:
+        n = len(word)
+        ans = 0
+        for i in range(n):
+            g = defaultdict(int)
+            t = 0
+            for j in range(i, n):
+                c = word[j]
+                if c in {'a', 'e', 'i', 'o', 'u'}:
+                    g[c] += 1
+                else:
+                    t += 1
+                if len(g.keys()) == 5 and t == k:
+                    ans += 1
+                if t > k:
+                    break
+        return ans
