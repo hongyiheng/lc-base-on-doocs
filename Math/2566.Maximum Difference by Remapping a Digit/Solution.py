7cba45372cf654c05f7f377e4af22326180a97c2
hongyiheng <hongyihengg@gmail.com>
2025-06-14 21:52:38 +0800

    feat: NO.2566.Maximum Difference by Remapping a Digit (Math)

---
+++ Math/2566.Maximum Difference by Remapping a Digit/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minMaxDifference(self, num: int) -> int:
+        s = str(num)
+        mx = num
+        for c in s:
+            if c != '9':
+                mx = int(str(num).replace(c, '9'))
+                break
+        s = s.replace(s[0], '0')
+        return mx - int(s)
+
+
+
