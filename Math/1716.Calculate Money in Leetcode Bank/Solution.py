cff91a3308d976f8813afa85af37500057248b68
hongyiheng <hongyihengg@gmail.com>
2025-10-25 21:33:07 +0800

    feat: NO.1716.Calculate Money in Leetcode Bank (Math)

--- Math/1716.Calculate Money in Leetcode Bank/Solution.py
+++ Math/1716.Calculate Money in Leetcode Bank/Solution.py
@@ -1,13 +1,3 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        week, rest = n // 7, n % 7
-        m = [1, 2, 3, 4, 5, 6, 7]
-        ans = 0
-        while week > 0:
-            for i, v in enumerate(m):
-                ans += v
-                m[i] += 1
-            week -= 1
-        for i in range(rest):
-            ans += m[i]
-        return ans
+      return sum(i // 7 + 1 + i % 7 for i in range(n))
